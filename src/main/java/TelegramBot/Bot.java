297c1f872b8c4627a0d50f80801f0ff8e47686a3
Kyrylo Zymarev <kyrylo.zymarev@software.nure.ua>
2018-05-29 21:03:46 +0300

    first test

---
+++ src/main/java/TelegramBot/Bot.java
@@ -0,0 +1,44 @@
+package TelegramBot;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.updatingmessages.DeleteMessage;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class Bot extends TelegramLongPollingBot {
+    @Override
+    public void onUpdateReceived(Update update) {
+        if(update.getMessage().getLeftChatMember()!=null || update.getMessage().getNewChatMembers()!=null){
+            DeleteMessage initDeleteMEssage = new DeleteMessage();
+            initDeleteMEssage.setChatId(update.getMessage().getChatId().toString());
+            initDeleteMEssage.setMessageId(update.getMessage().getMessageId());
+            try{
+                deleteMessage(initDeleteMEssage);
+            }catch(TelegramApiException ex){
+
+            }
+        }
+    }
+//some text
+    @Override
+    public String getBotUsername() {
+        return "@NotificationRemover_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "557298333:AAGqw-RWv_jcTsRi6OJAHq7aZMBPiI-8X3Q";
+    }
+
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new Bot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
